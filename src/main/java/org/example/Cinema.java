9c09bdcd91842f8928d8bbde54c20e696ff039a3
Mark Bilous <belousme601@gmail.com>
2023-10-25 10:00:50 +0300

    method bookSeats (50% done)

--- src/main/java/org/example/Cinema.java
+++ src/main/java/org/example/Cinema.java
@@ -1,20 +1,53 @@
 package org.example;
 
 public class Cinema {
-    public static void main(String[] args) {
-        int halls = 5;
-        int rows = 10;
-        int seatsPerRow = 20;
+    private static int halls = 5;
+    private static int rows = 10;
+    private static int seatsPerRow = 20;
 
-        int[][][] cinemaArray = new int[halls][rows][seatsPerRow];
+    private static int[][][] cinemaArray;
 
-        for(int hall = 0; hall < halls; hall++){
-            for(int row = 0; row < rows; row++) {
-                for(int seat = 0; seat < seatsPerRow; seat++){
+    public Cinema() {
+
+        cinemaArray = new int[halls][rows][seatsPerRow];
+
+        for (int hall = 0; hall < halls; hall++) {
+            for (int row = 0; row < rows; row++) {
+                for (int seat = 0; seat < seatsPerRow; seat++) {
                     cinemaArray[hall][row][seat] = 0;
                 }
             }
         }
+    }
+
+    public  void bookSeats(int hallNumber, int row, int[] seats) {
+        if (hallNumber < 1 || hallNumber > halls) {
+            System.out.println("Зал з номером " + hallNumber + " не існує");
+            return;
+        }
+
+        if(row < 1 || row > rows) {
+            System.out.println("Ряд " + row + " не існує в залі " +  hallNumber );
+            return;
+        }
+
+        for (int seat : seats) {
+            if (seat < 1 || seat > seatsPerRow) {
+                System.out.println("Місце " + seat + " не існує в ряду " +  row + "." );
+                return;
+            }
+
+            int seatIndex = seat - 1;
+            if (cinemaArray[hallNumber - 1][row - 1][seatIndex] == 1) {
+                System.out.println("Місце " + seat + " в ряду "  + row + " в залі " + hallNumber + " вже заброньоване.");
+            } else {
+                cinemaArray[hallNumber - 1][row - 1][seatIndex] = 1;
+                System.out.println("Місце " + seat + " в ряду " + row + " в залі " + hallNumber + "було успішно заброньоване.");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Cinema cinema = new Cinema();
 
         for(int hall = 0; hall < halls; hall++){
             System.out.println("Зал" + (hall + 1));
